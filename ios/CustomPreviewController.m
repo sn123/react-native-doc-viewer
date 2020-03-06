c9ab5d4d554994befb17a5f03679d5bd7f371751
madhanagopal78 <madhanagopal@gmail.com>
2020-03-06 11:32:53 +0530

    Update CustomPreviewController.m (#1)
    Reduced the timer to 0.25

--- ios/CustomPreviewController.m
+++ ios/CustomPreviewController.m
@@ -18,7 +18,7 @@ NSTimer *timer;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     if ([_hideShareButton  isEqual: @"1"]) {
-        timer = [NSTimer scheduledTimerWithTimeInterval: 1.0
+        timer = [NSTimer scheduledTimerWithTimeInterval: 0.25
                                                  target: self
                                                selector:@selector(onTick:)
                                                userInfo: nil repeats:YES];
